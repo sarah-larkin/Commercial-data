dee4ead1e6f71e0cc9e6b36adc70587d7ff32c3d
sarah-larkin <larkinasarah@gmail.com>
2026-04-09 11:41:17 +0100

    dim table setup

--- super_store/sql/tables.sql
+++ super_store/sql/tables.sql
@@ -1,33 +1,49 @@
 \c superstore
 
--- Create Locations tables : market, region, state
+""" The dims tables contain multiple columns. 
+These could be separated out further. 
+Given the size of the dataset and purpose of this project
+they will remain combined at this point."""
 
-CREATE TABLE market (
-    market_id SERIAL PRIMARY KEY,
-    market_name TEXT
-);
+-- Create Locations table : market, region, state
 
-CREATE TABLE region (
-    region_id SERIAL PRIMARY KEY,
-    region_name TEXT
-);
-
-CREATE TABLE state (
-    state_id SERIAL PRIMARY KEY,
+CREATE TABLE dim_location (
+    location_id SERIAL PRIMARY KEY,
+    market_name TEXT,
+    region_name TEXT,
     state_name TEXT
 );
 
--- create Date tables : dates (one or 2 for order and shipping?)
+-- create Date table : date range, date, quarter etc. 
+-- (this would only need to be created once and not updated)
+CREATE TABLE dim_date (
+    #TODO: decide date table format
+);
 
--- create Product tables: product_id & product_name, category, sub-category,  segment
+-- create Product table: product_id & product_name, category, sub-category,  segment
+--(data set is small enough that this can be in one table)
 
--- create Customer tables: customer_id & Customer_name
+CREATE TABLE dim_product (
+    product_id TEXT,
+    product_name TEXT,
+    category TEXT,
+    sub-category TEXT
+);
 
--- create Shippinge tables: shipping_mode, order priority 
+
+-- create Customer table: customer_id & Customer_name
+
+CREATE TABLE dim_customer (
+    customer_id SERIAL PRIMARY KEY,
+    customer_name TEXT,
+    segment TEXT
+);
 
 
 -- create fact table with all new columns
 
-
+CREATE TABLE fact_orders (
+    #TODO: complete fact table
+);
 
 """psql sql/tables.sql"""
